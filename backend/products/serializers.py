757a2799b1b8bbe4ca973b77ded1fcf318e4cc71
Gentra Ruswanda <gentra.a@gmail.com>
2019-04-16 01:43:54 +0700

    Show auctions_start_at and auctions_end_at for Products

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -8,7 +8,8 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'desc', 'price', 'bid_init_price', 'bid_current_price', 'image', 'points_to_deduct')
+        fields = ('id', 'name', 'desc', 'price', 'bid_init_price', 'bid_current_price', 'image', 'points_to_deduct',
+                  'auction_start_at', 'auction_end_at')
 
     def get_points_to_deduct(self, obj):
         return Bid.calculate_point(obj.bid_current_price)
