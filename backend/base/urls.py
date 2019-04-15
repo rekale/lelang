8402fd2e792ce056c47d5149b08738e15bc75103
muhamad haekal <rekale1123@gmail.com>
2019-04-15 20:23:48 +0700

    create points for user

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from quickstart import views
 from django.conf import settings
 from django.conf.urls.static import static
 from products import views as viewsProduct
 from bids import views as viewsBid
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
+admin.site.site_header = "Lelang Tokopedia"
+admin.site.site_title = "Lelang Tokopedia"
+admin.site.index_title = "Welcome to Tokopedia"
+
+router = routers.DefaultRouter()
 router.register(r'products', viewsProduct.ProductViewSet)
 router.register(r'bids', viewsBid.BidViewSet)
 router.register(r'bid_options', viewsBid.BidOptionViewSet)
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
