234cf2fecd7ded77c20d09a0d03931927f68903e
Gentra Ruswanda <gentra.a@gmail.com>
2019-04-16 01:40:42 +0700

    Add start_at and end_at for product

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
@@ -9,6 +8,8 @@ class Product(models.Model):
     bid_init_price = models.IntegerField(default=0)
     bid_current_price = models.IntegerField(default=0)
     image = models.FileField(upload_to='products')
+    auction_start_at = models.DateTimeField(default=None, null=True)
+    auction_end_at = models.DateTimeField(default=None, null=True)
 
     def __str__(self):
         return self.name
