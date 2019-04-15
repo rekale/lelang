8305feb22615c9d4c8b304ca8e6137b3c8f65372
Gentra Ruswanda <gentra.a@gmail.com>
2019-04-16 01:20:04 +0700

    Show points_to_deduct on product's page

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,8 +1,14 @@
+from bids.models import Bid
 from .models import Product
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    points_to_deduct = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'desc', 'price', 'bid_init_price', 'bid_current_price', 'image')
+        fields = ('id', 'name', 'desc', 'price', 'bid_init_price', 'bid_current_price', 'image', 'points_to_deduct')
+
+    def get_points_to_deduct(self, obj):
+        return Bid.calculate_point(obj.bid_current_price)
