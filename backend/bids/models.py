587db751f1bc3fc3cfc73e6920768b033183eab2
Gentra Ruswanda <gentra.a@gmail.com>
2019-04-15 23:07:50 +0700

    Update Product's Bid_Current_Price when creating a new bid

--- backend/bids/models.py
+++ backend/bids/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from products.models import Product
 
 
@@ -15,8 +18,19 @@ class Bid(models.Model):
         return float(self.offering_price) / 10
 
 
+# Update Product's Bid_Current_Price when creating a new bid
+@receiver(post_save, sender=Bid)
+def save_bid_updates_current_price(sender, instance, created, **kwargs):
+    if created:
+        product = Product.objects.get(pk=instance.product_id)
+        product.bid_current_price = instance.offering_price
+        product.save()
+
+
 class BidOption(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price_increment = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
