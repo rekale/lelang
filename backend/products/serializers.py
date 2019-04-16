56a348efbaf4597ac7df204b55e9250c6fd46db9
Gentra Ruswanda <gentra.a@gmail.com>
2019-04-16 09:20:13 +0700

    Add "bid_status_label"

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -5,11 +5,19 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     points_to_deduct = serializers.SerializerMethodField()
+    bid_status_label = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'desc', 'price', 'bid_init_price', 'bid_current_price', 'image', 'points_to_deduct',
-                  'auction_start_at', 'auction_end_at')
+                  'auction_start_at', 'auction_end_at', 'bid_status_label')
 
     def get_points_to_deduct(self, obj):
         return Bid.calculate_point(obj.bid_current_price)
+
+    def get_bid_status_label(self, obj):
+        bid_count = Bid.objects.filter(product_id=obj.id).count()
+        if bid_count > 0:
+            return "Telah ditawar"
+
+        return "Ajukan lelang"
